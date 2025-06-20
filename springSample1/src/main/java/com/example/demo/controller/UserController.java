485e6acf61bf91ab053cc42f6f5bfacb21bc3008
user <user@DESKTOP-IOCFTSU>
2025-06-20 18:14:53 +0900

    6/20 18時

--- springSample1/src/main/java/com/example/demo/controller/UserController.java
+++ springSample1/src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 
@@ -17,6 +16,7 @@ import com.example.demo.service.UserService;
  */
 @Controller
 public class UserController {
+
 	/**
 	 * ユーザー情報 Service
 	 */
@@ -24,26 +24,14 @@ public class UserController {
 	UserService userService;
 
 	/**
-	 * ユーザー情報検索画面を表示
+	 * ユーザー情報一覧画面を表示
 	 * @param model Model
-	 * @return ユーザー情報一覧画面
+	 * @return ユーザー情報一覧画面のHTML
 	 */
-	@GetMapping(value = "/user/search")
-	public String displaySearch(Model model) {
-		model.addAttribute("userSearchRequest", new UserSearchRequest());
-		return "user/search";
-	}
-
-	/**
-	 * ユーザー情報検索
-	 * @param userSearchRequest リクエストデータ
-	 * @param model Model
-	 * @return ユーザー情報一覧画面
-	 */
-	@RequestMapping(value = "/user/id_search", method = RequestMethod.POST)
-	public String search(@ModelAttribute UserSearchRequest userSearchRequest, Model model) {
-		User user = userService.search(userSearchRequest);
-		model.addAttribute("userinfo", user);
-		return "user/search";
+	@RequestMapping(value = "/user/list", method = RequestMethod.GET)
+	public String displayList(Model model) {
+		List<User> userlist = userService.searchAll();
+		model.addAttribute("userlist", userlist);
+		return "user/list";
 	}
 }
--- springSample1/src/main/java/com/example/demo/service/UserService.java
+++ springSample1/src/main/java/com/example/demo/service/UserService.java
@@ -1,29 +1,27 @@
 package com.example.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.dto.UserSearchRequest;
 import com.example.demo.entity.User;
-import com.example.demo.repository.UserMapper;
+import com.example.demo.repository.UserRepository;
 
 /**
  * ユーザー情報 Service
  */
 @Service
 public class UserService {
-	/**
-	 * ユーザー情報 Mapper
-	 */
-	@Autowired
-	private UserMapper userMapper;
 
 	/**
-	 * ユーザー情報検索
-	　　　* @param userSearchRequest リクエストデータ
-	 * @return 検索結果
+	 * ユーザー情報 Repository
 	 */
-	public User search(UserSearchRequest userSearchRequest) {
-		return userMapper.search(userSearchRequest);
+	@Autowired
+	UserRepository userRepository;
+
+	public List<User> searchAll() {
+		// ユーザーTBLの内容を全検索
+		return userRepository.findAll();
 	}
 }
